b2f7f93ee9f24faa45a4c21b581307a7ab1c23a3
AdamKniec <adam.k.kniec@gmail.com>
2020-01-06 22:22:00 +0100

    adding some minor css fixes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,10 +8,8 @@
       if (e.target === menuItem) {
         descriptionBoxes.forEach((box, index) => {
           box.classList.add("hide");
-          // box.classList.remove("show");
           if (i === index) {
             box.classList.remove("hide");
-            // box.classList.remove("show");
           }
         });
       }
@@ -19,33 +17,6 @@
   };
   aboutMeMenuWrapper.addEventListener("mouseover", handleHover);
 })();
-
-//onResize, onload  class manipulation
-
-// (function() {
-//   const descriptionBoxes = document.querySelectorAll(".description-box");
-
-//   const handleClassChangeOnLoad = () => {
-//     descriptionBoxes.forEach(box => {
-//       box.classList.add("hide");
-//       descriptionBoxes[0].classList.remove("hide");
-//     });
-//   };
-
-//   const handleResize = () => {
-//     const screenWidth = window.innerWidth;
-//     descriptionBoxes.forEach(box => {
-//       if (screenWidth <= 1000) {
-//         box.classList.remove("hide");
-//       } else {
-//         box.classList.add("hide");
-//         descriptionBoxes[0].classList.remove("hide");
-//       }
-//     });
-//   };
-//   // window.addEventListener("load", handleClassChangeOnLoad);
-//   window.addEventListener("resize", handleResize);
-// })();
 
 //onResize, onLoad class manipulation
 (function() {
@@ -66,7 +37,6 @@
   window.addEventListener("resize", manipulateBoxClasses);
 })();
 // Navbar onScroll handler
-
 (function() {
   const navbar = document.querySelector(".navbar");
   const introContainer = document.querySelector(".introContainer");
@@ -81,21 +51,17 @@
   });
 })();
 // Hamburger menu handler
-
 (function() {
   const button = document.querySelector(".hamburger");
   const navList = document.querySelector(".navbar-list");
   const body = document.querySelector("body");
   const handleHamburgerClick = e => {
     if (e.target === button) {
-      console.log("button");
       navList.classList.add("mobile");
     } else if (e.target !== navList) {
-      console.log("nie button");
       navList.classList.remove("mobile");
     }
   };
-
   body.addEventListener("click", handleHamburgerClick);
 })();
 
@@ -103,7 +69,6 @@
 (function() {
   $(document).ready(function() {
     var owl = $(".owl-carousel");
-
     owl.owlCarousel({
       items: 1,
       loop: true,
@@ -111,8 +76,6 @@
       autoplay: true,
       autoplayTimeout: 3000,
       autoplayHoverPause: true
-      // nav: true
-      // dots: true
     });
   });
 })();
